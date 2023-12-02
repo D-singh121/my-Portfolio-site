13bedc8fa4476182be38976cef677f58d88cc57e
Devesh_singh <choudharydevesh121@gmail.com>
2023-12-02 16:38:01 +0530

    Done with service section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './app.scss';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/herosection/Hero';
 import Parallax from './components/parallax/Parallax';
+import Services from './components/services/Services';
 const App = () => {
   return (
     <>
@@ -9,9 +10,9 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id='services'><Parallax type="services"/></section>
-      <section> Service</section>
-      <section id='portfolio'><Parallax type="portfolio"/></section>
+      <section id='services'><Parallax type="services" /></section>
+      <section> <Services /></section>
+      <section id='portfolio'><Parallax type="portfolio" /></section>
       <section>Portfolio1</section>
       <section>POrtfolio2</section>
       <section>Portfolio3</section>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,104 @@
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react";
+import "./services.scss"
+
+
+const variants = {
+	initial: {
+		x: -500,
+		y: 100,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 1,
+			staggerChildren: 0.1,
+		},
+	},
+};
+
+const Services = () => {
+	const ref = useRef();
+
+	const isInView = useInView(ref, { margin: "-100px" });
+
+	return (
+		<motion.div
+			ref={ref}
+			className="services"
+			variants={variants}
+			initial="initial"
+			animate={isInView && "animate"}>
+
+			<motion.div className="textContainer" variants={variants}>
+				<p>
+					We focus on helping your brand <br /> grow and move forward
+				</p>
+				<hr />
+			</motion.div>
+
+			<motion.div className="titleContainer" variants={variants}>
+				<div className="title">
+					<img src="../../../public/people.webp" alt="people" />
+					<h1><motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas</h1>
+				</div>
+
+				<div className="title">
+					<h1><motion.b whileHover={{ color: "orange" }}>For Your</motion.b> Business.</h1>
+					<button>WHAT WE DO </button>
+				</div>
+			</motion.div>
+
+			{/* //***** list box section */}
+			<motion.div className="listContainer" variants={variants}>
+				<motion.div whileHover={{ background: "lightgray", color: "black" }} className="box" >
+					<h2>Branding</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+						libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+						animi! Ad nam p
+					</p>
+					<button>Do Check</button>
+				</motion.div>
+
+				<motion.div whileHover={{ background: "lightgray", color: "black" }} className="box" variants={variants}>
+					<h2>Branding</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+						libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+						animi! Ad nam p
+					</p>
+					<button>Do Check</button>
+				</motion.div>
+
+				<motion.div whileHover={{ background: "lightgray", color: "black" }} className="box" >
+					<h2>Branding</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+						libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+						animi! Ad nam p
+					</p>
+					<button>Do Check</button>
+				</motion.div>
+
+				<motion.div whileHover={{ background: "lightgray", color: "black" }} className="box" >
+					<h2>Branding</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+						libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+						animi! Ad nam p
+
+					</p>
+					<button>Do Check</button>
+				</motion.div>
+			</motion.div>
+
+		</motion.div>
+
+	)
+}
+
+export default Services
